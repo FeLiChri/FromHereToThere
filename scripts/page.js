9281007ad6e19633c9f4c6361a011d04c51d4000
Fee Christoph <felichri@umich.edu>
2021-11-16 12:09:41 -0500

    in progress save

--- scripts/page.js
+++ scripts/page.js
@@ -24,6 +24,10 @@ map = new Vue({
       },
       // TODO: diff between current hunt being made vs. being played?
       currHunt: {
+        expectedDistance: 0,
+        expectedTime: 0,
+        title: "",
+
         inProgress: {
           timeSoFar: 0,
           distanceSoFar: 0,
@@ -35,75 +39,113 @@ map = new Vue({
           tryAgain: false,
           correct: false,
         },
-        expectedDistance: 0,
-        expectedTime: 0,
-        stops: [
-          {
-            clue: "Where the graduates study.",
-            answer: "Hatcher",
-            hint: "It starts with 'hat'",
-            task: "Take a photo in the stacks.",
-            points: 15,
-            latlong: L.latLng(42.2808, -83.7430),
-          },
-          {
-            clue: "Where to find computers in Angell Hall.",
-            answer: "Fishbowl",
-            hint: "Nemo from Finding Nemo might swim in one.",
-            task: "Print a poster and upload a photo of the print!",
-            points: 15,
-            latlong: L.latLng(42.2808, -83.7430),
-          },
-        ],
+        stops: [],
       },
       allHunts: [],      
     };
   },
   mounted() {
-  	const map = this.$refs.mymap.mapObject;
-    routermap = map;
+  	const this_map = this.$refs.mymap.mapObject;
+    routermap = this_map;
     // map.addControl(new L.Control.Fullscreen());
+
+    console.log(this.makeMarkers);
+    this.router = L.Routing.control({
+      // TODO: check waypoints appearing
+      waypoints: this.makeMarkers,   
+      routeWhileDragging: false,
+      // TODO: fix dragging problem
+      lineOptions: {
+        addWaypoints: false,
+      },
+      show: false,
+      units: 'imperial',
+      // summaryTemplate: '<h2>{name}</h2><h3>{distance}, {time}</h3>',
+    });
+        
+    this.router.on('routesfound', function(e) {
+        console.log("ROUTES FOUND");
+        console.log(e.waypoints);
+        var routes = e.routes;
+        var summary = routes[0].summary;
+        // alert distance and time in miles and minutes
+        console.log(summary.totalDistance / 1760 + ' mi');
+        console.log(summary.totalTime % 3600 / 60 + ' min');
+        map.currHunt.expectedTime = (summary.totalTime % 3600 / 60).toFixed(2);
+        map.currHunt.expectedDistance = (summary.totalDistance / 1760).toFixed(2);
+        // alert('Total distance is ' + summary.totalDistance / 1000 + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
+    });
+    
+    this.router.addTo(routermap);
   },
   methods: {
     registerMarker: function(e) {
         console.log(e);
     },
     setTimeAndDistance: function(time, distance) {
+        console.log(time, distance);
         this.currHunt.expectedDistance = distance.toFixed(2);
         this.currHunt.expectedTime = time.toFixed(2);
     },
+    addStop: function() {
+      this.currHunt.stops.push( {
+        clue: "",
+        answer: "",
+        hint: "",
+        task: "",
+        points: 15,
+        latlong: L.latLng(42.2766, -83.7397),
+        expanded: true,
+      });
+    },
+    publish: function() {
+      // TODO: error checking!
+    }
   }, 
   computed: {
     makeMarkers: function() {
-      return this.currHunt.stops.filter(s => s.latlong);
+      console.log(this.currHunt.stops.map(s => s.latlong));
+      return this.currHunt.stops.map(s => s.latlong);
     },
     guessMarkers: function() {
       return this.makeMarkers.slice(0, this.currHunt.inProgress.currStop - 1);
-    }
+    }, 
   }
 });
 
+console.log("I think Vue just rendered?");
+console.log(map.makeMarkers);
 
-var router = L.Routing.control({
-    // TODO: check waypoints appearing
-    waypoints: map.makeMarkers,   
-    routeWhileDragging: false,
-    addWaypoints: false,
-    show: false,
-    units: 'imperial',
-    // summaryTemplate: '<h2>{name}</h2><h3>{distance}, {time}</h3>',
 
-});
 
-router.on('routesfound', function(e) {
-    var routes = e.routes;
-    var summary = routes[0].summary;
-    // alert distance and time in km and minutes
-    console.log(summary.totalDistance / 1760 + ' mi');
-    console.log(summary.totalTime % 3600 / 60 + ' min');
-    map.setTimeAndDistance(summary.totalTime % 3600 / 60, summary.totalDistance / 1760);
-    // alert('Total distance is ' + summary.totalDistance / 1000 + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
- });
-
-router.addTo(routermap);
-
+/*
+stops: [
+  {
+    clue: "Where the graduates study.",
+    answer: "Hatcher",
+    hint: "It starts with 'hat'",
+    task: "Take a photo in the stacks.",
+    points: 15,
+    latlong: L.latLng(42.2808, -83.7430),
+    expanded: true,
+  },
+  {
+    clue: "Where to find computers in Angell Hall.",
+    answer: "Fishbowl",
+    hint: "Nemo from Finding Nemo might swim in one.",
+    task: "Print a poster and upload a photo of the print!",
+    points: 15,
+    latlong: L.latLng(42.2766, -83.7397),
+    expanded: true,
+  },
+  {
+    clue: "",
+    answer: "",
+    hint: "",
+    task: "",
+    points: 15,
+    latlong: L.latLng(42.2766, -83.7397),
+    expanded: true,
+  },
+],
+*/
